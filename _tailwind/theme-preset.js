2ae4e560756ed46ae5f702ef42a5229c7c08faa5
Mathew <musomatty@gmail.com>
2026-04-28 18:35:08 +1000

    Initial commit

---
+++ _tailwind/theme-preset.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1033a4', // Example primary color
+                secondary: '#cfd8dc', // Example secondary color
+                tertiary: '#22ccfb', // Example secondary color
+            }
+        }
+    }
+}
